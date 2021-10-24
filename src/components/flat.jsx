4c36ded0bf8c1cf64776416e3f2d1c2637010c9a
Louis Drinkwater <izumilouis95@gmail.com>
2021-10-24 23:30:12 +0100

    Fix style errors

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -6,18 +6,29 @@ class Flat extends Component {
   }
 
   render() {
-    const { id, name, imageUrl, price, priceCurrency, lat, lng } = this.props.details;
+    const {
+      id,
+      name,
+      imageUrl,
+      price,
+      priceCurrency,
+      lat,
+      lng
+    } = this.props.details;
     return (
       <div
         className="card"
         style={{ background: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url("${imageUrl}")`, }}
         onClick={this.handleClick}
-        >
-        <div className="card-category">{price} {priceCurrency}</div>
+      >
+        <div className="card-category">
+          {price}
+          {priceCurrency}
+        </div>
         <div className="card-description">
           <h2>{name}</h2>
         </div>
-        <a className="card-link" href="#"></a>
+        <a className="card-link" href="#" />
       </div>
     );
   }
